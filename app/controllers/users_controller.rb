a313d6efc6143c4020890e58b80ecd66a90db250
EC2 Default User <ec2-user@ip-172-31-47-72.ap-northeast-1.compute.internal>
2020-03-16 12:33:09 +0000

    リスト 7.22: エラーメッセージにスタイルを与えるためのCSS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # notice: '登録が完了しました。'
+    else
+      render 'new', notice: '登録内容に誤りがあります。'
+    end
+  end
+
+  private
+    def user_params
+        params.require(:user).permit(:name, :email, :password,
+        :password_confirmation)
+    end
+
 end
